65ae061606fb5c9df2e931a316bc73d6c2614e1b
creeder23 <creeder@DGCarlReeder.local>
2016-09-15 11:40:23 -0600

    photo update

--- runtime/Protogen_runtime.py
+++ runtime/Protogen_runtime.py
@@ -19,7 +19,7 @@ gluetask3 = gbdx.Task('gdal-cli')
 gluetask3.inputs.data = WV3
 gluetask3.inputs.execution_strategy = 'runonce'
 gluetask3.inputs.command = """mv $indir/*/*.tif $outdir/"""
-
+'''
 protoLULC = gbdx.Task("protogenV2LULC", raster=gluetask2.outputs.data.value)
 protoPAN = gbdx.Task("protogenV2PANTEX10", raster=gluetask2.outputs.data.value)
 protoRAC = gbdx.Task("protogenV2RAC", raster=gluetask2.outputs.data.value)
@@ -27,7 +27,7 @@ protoRAS = gbdx.Task("protogenV2RAS", raster=gluetask2.outputs.data.value)
 protoRAV = gbdx.Task("protogenV2RAV", raster=gluetask2.outputs.data.value)
 protoRAW = gbdx.Task("protogenV2RAW", raster=gluetask2.outputs.data.value)
 
-
+'''
 protoLULC3 = gbdx.Task("protogenV2LULC", raster=gluetask3.outputs.data.value)
 protoPAN3 = gbdx.Task("protogenV2PANTEX10", raster=gluetask3.outputs.data.value)
 protoRAC3 = gbdx.Task("protogenV2RAC", raster=gluetask3.outputs.data.value)
@@ -35,8 +35,10 @@ protoRAS3 = gbdx.Task("protogenV2RAS", raster=gluetask3.outputs.data.value)
 protoRAV3 = gbdx.Task("protogenV2RAV", raster=gluetask3.outputs.data.value)
 protoRAW3 = gbdx.Task("protogenV2RAW", raster=gluetask3.outputs.data.value)
 
-workflow = gbdx.Workflow([ gluetask2, gluetask3, protoLULC, protoPAN, protoRAC, protoRAS, protoRAV, protoRAW, protoLULC3, protoPAN3, protoRAC3, protoRAS3, protoRAV3, protoRAW3 ])
 
+workflow = gbdx.Workflow([ gluetask3, protoLULC3, protoPAN3, protoRAC3, protoRAS3, protoRAV3, protoRAW3 ])
+
+'''
 workflow.savedata(protoLULC.outputs.data, location="Benchmark/Protogen/LULC")
 workflow.savedata(protoPAN.outputs.data, location="Benchmark/Protogen/PAN")
 workflow.savedata(protoRAC.outputs.data, location="Benchmark/Protogen/RAC")
@@ -44,6 +46,7 @@ workflow.savedata(protoRAS.outputs.data, location="Benchmark/Protogen/RAS")
 workflow.savedata(protoRAV.outputs.data, location="Benchmark/Protogen/RAV")
 workflow.savedata(protoRAW.outputs.data, location="Benchmark/Protogen/RAW")
 
+'''
 workflow.savedata(protoLULC3.outputs.data, location="Benchmark/Protogen/LULC3")
 workflow.savedata(protoPAN3.outputs.data, location="Benchmark/Protogen/PAN3")
 workflow.savedata(protoRAC3.outputs.data, location="Benchmark/Protogen/RAC3")
